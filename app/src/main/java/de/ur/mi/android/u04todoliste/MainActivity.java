d4ab6c1e5e34e70840d5f149b9b8f7b4510f6d8d
Tim Eichinger <business.timeichinger@gmail.com>
2020-04-03 10:22:31 +0200

    Shapes für ImageButton hinzugefügt und Settings angepasst

--- app/src/main/java/de/ur/mi/android/u04todoliste/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/u04todoliste/MainActivity.java
@@ -1,4 +1,4 @@
-package de.ur.mi.android.base;
+package de.ur.mi.android.u04todoliste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
